2c035e0131ff8fee3a9f3fc6c102f9bb73b07bff
jason870721 <jason6096096@gmail.com>
2025-12-13 17:11:43 +0800

    feature: Create server and connection

--- deck/deck.go
+++ deck/deck.go
@@ -1,6 +1,27 @@
 package deck
 
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
 type Suit int
+
+func (s Suit) String() string {
+	switch s {
+	case Spades:
+		return "SPADES"
+	case Harts:
+		return "HARTS"
+	case Diamonds:
+		return "DIAMONDS"
+	case Clubs:
+		return "CLUBS"
+	default:
+		panic("invalid card suit")
+	}
+}
 
 const (
 	Spades Suit = iota
@@ -14,6 +35,14 @@ type Card struct {
 	value int
 }
 
+func (c Card) String() string {
+	value := strconv.Itoa(c.value)
+	if c.value == 1 {
+		value = "Ace"
+	}
+	return fmt.Sprintf("%s of %s %s", value, c.suit, suitToUnicode(c.suit))
+}
+
 func NewCard(s Suit, v int) Card {
 	if v > 13 {
 		panic("The value of card cannot be higher than 13")
@@ -21,5 +50,50 @@ func NewCard(s Suit, v int) Card {
 	return Card{
 		suit:  s,
 		value: v,
+	}
+}
+
+type Deck [52]Card
+
+func New() Deck {
+	var (
+		nSuits = 4
+		nCards = 13
+		d      = [52]Card{}
+	)
+
+	x := 0
+	for i := 0; i < nSuits; i++ {
+		for j := 0; j < nCards; j++ {
+			d[x] = NewCard(Suit(i), j+1)
+			x++
+		}
+	}
+	return Shuffle(d)
+}
+
+func Shuffle(d Deck) Deck {
+	for i := 0; i < len(d); i++ {
+		r := rand.Intn(i + 1)
+
+		if r != i {
+			d[i], d[r] = d[r], d[i]
+		}
+	}
+	return d
+}
+
+func suitToUnicode(s Suit) string {
+	switch s {
+	case Spades:
+		return "♠"
+	case Harts:
+		return "♥"
+	case Diamonds:
+		return "♦"
+	case Clubs:
+		return "♣"
+	default:
+		panic("invalid card suit")
 	}
 }
--- main.go
+++ main.go
@@ -3,10 +3,38 @@ package main
 import (
 	"fmt"
 
-	"github.com/jason870721/GGPoker/deck"
+	"github.com/jason870721/GGPoker/p2p"
 )
 
 func main() {
-	card := deck.NewCard(deck.Spades, 1)
-	fmt.Println(card)
+	// ================================================
+	// following code mock a remote gamer's PC
+	// like other player start a client on his/her computer.
+	remoteUserCfg := p2p.ServerConfig{
+		Name:       "Stevie",
+		Version:    "GGPOKER V0.1-alpha",
+		ListenAddr: ":3000",
+	}
+	remoteUserClient := p2p.NewServer(remoteUserCfg)
+	remoteUserClient.Start()
+	// ================================================
+
+	// ================================================
+	// start my PC client and try to connect to other player who listen on 3000 port.
+	myCfg := p2p.ServerConfig{
+		Name:       "Kai",
+		Version:    "GGPOKER V0.1-alpha",
+		ListenAddr: ":4000",
+	}
+	myClient := p2p.NewServer(myCfg)
+	myClient.Start()
+
+	if err := myClient.Connect(":3000"); err != nil {
+		fmt.Printf("connect failed: %v", err)
+	}
+
+	for {
+	}
+
+	//fmt.Println(deck.New())
 }
